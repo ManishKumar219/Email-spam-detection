8d41df363f81ea9153018b3b85304dc5329aca5c
ManishKumar219 <mk8051manish@gmail.com>
2023-04-15 00:00:42 +0530

    Commit web application

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
 import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+cnt_vec = pickle.load(open("model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict_api', methods=['POST'])
+
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print([data])
+    new_data = cnt_vec.transfrom([data])
+    output = model.predict(new_data)
+    print(output)
+
+if __name__ == "__main__":
+     app.run(debug=True)
