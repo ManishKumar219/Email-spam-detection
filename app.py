22ccf70e86948a6735b61f109ee98730181a7c86
ManishKumar219 <mk8051manish@gmail.com>
2023-04-15 15:30:20 +0530

    Complete web appication

--- app.py
+++ app.py
@@ -6,22 +6,28 @@ import pandas as pd
 app = Flask(__name__)
 
 model = pickle.load(open("model.pkl", "rb"))
-cnt_vec = pickle.load(open("model.pkl", "rb"))
+cnt_vec = pickle.load(open("count_vectorizer.pkl", "rb"))
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/predict_api', methods=['POST'])
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [request.form['text']]
+    t_data = cnt_vec.transform(data)
+    output = model.predict(t_data)
 
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print([data])
-    new_data = cnt_vec.transfrom([data])
-    output = model.predict(new_data)
-    print(output)
+    if(len(data[0])==0):
+        return render_template('home.html', msg = "Enter your e-mail text above...")
+    
+    if output=='ham':
+        return render_template('home.html', ham = "NO SPAM")
+    else:
+        return render_template('home.html', spam = "SPAM MAIL")
+    
+
 
 if __name__ == "__main__":
-     app.run(debug=True)
+    app.run(debug=True)   
